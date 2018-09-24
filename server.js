0db76e7b0e093259345808245efbc620b02a8ef3
mattko <marekpetak@users.noreply.github.com>
2018-09-24 12:36:45 +0100

    add api route /api/whoami
    as per fcc user cases

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  //res.json({test: req.headers});
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'].split(',')[0], 
+    language: req.headers['accept-language'], 
+    software: req.headers['user-agent']});
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
